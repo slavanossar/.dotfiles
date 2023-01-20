ba25927430362c5a8f65a854706f50ecba54be8f
Slava Nossar <slavanossar@users.noreply.github.com>
2023-01-20 13:52:43 +1100

    remove WP CLI and Composer install scripts

--- setup.sh
+++ setup.sh
@@ -8,30 +8,3 @@ xcode-select --install
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-# WP CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-php wp-cli.phar --info
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
-
-# Composer
-EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-chmod +x composer.phar
-sudo mv composer.phar /usr/local/bin/composer
-unset EXPECTED_CHECKSUM
-unset ACTUAL_CHECKSUM
-exit $RESULT
